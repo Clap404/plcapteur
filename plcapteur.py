159044d7c9606ad0a83d812d97aab8552c68a7e9
Anthony Ruhier <anthony.ruhier@gmail.com>
2014-03-24 02:46:37 +0100

    Every areas has in minimum 1 sensor

--- plcapteur.py
+++ plcapteur.py
@@ -4,33 +4,30 @@ import random
 from pprint import pprint
 
 
-def gen_capteurs(config):
-    capteurs = []
-    for i in range(config["nb_capteur"]):
-        capteurs.append(random.sample(range(config["nb_zone"]),
-                        config["zone_par_capteur"]))
-    for i in range(len(capteurs)):
-        capteurs[i] = [random.randint(1, 10), capteurs[i]]
-        capteurs[i][1].sort()
-    return capteurs
-
-
-def groupby_zones(capteurs, config):
-    zones = dict()
+def gen_zones(config):
+    zones = []
     for i in range(config["nb_zone"]):
-        zones[i] = [capteurs.index(capteur) for capteur in capteurs
-                    if capteur[1].count(i)]
-        test = [0]
-        if len(zones[i]):
-            test = [capteurs[x][0] for x in zones[i]]
-        zones[i] = [max(test), zones[i]]
+        zones.append(random.sample(range(config["nb_capteur"]),
+                                   random.randint(1,
+                                                  config["zone_par_capteur"])))
+    for zone in zones:
+        zone.sort()
     return zones
+
+
+def groupby_capteur(zones, config):
+    capteurs = dict()
+    for i in range(config["nb_capteur"]):
+        capteurs[i] = [zones.index(zone) for zone in zones
+                       if zone.count(i)]
+        capteurs[i] = [random.randint(1, 10), capteurs[i]]
+    return capteurs
 
 
 with open('config.json') as data_file:
     config = json.load(data_file)
 pprint(config)
-capteurs = gen_capteurs(config)
-zones = groupby_zones(capteurs, config)
+zones = gen_zones(config)
+capteurs = groupby_capteur(zones, config)
 print("Capteurs : " + str(capteurs))
 print("Zones : " + str(zones))
