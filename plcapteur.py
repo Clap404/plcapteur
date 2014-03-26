5557121baec8d624b1e4d02c6780fd7658e668d5
Anthony Ruhier <anthony.ruhier@gmail.com>
2014-03-26 21:43:36 +0100

    Fix groupby_capteurs

--- plcapteur.py
+++ plcapteur.py
@@ -3,7 +3,10 @@ import json
 import random
 from pprint import pprint
 
-#TODO zone_par_caapteur
+
+configurations = []
+
+
 def gen_zones(config):
     zones = []
     for i in range(config["nb_zone"]):
@@ -15,13 +18,30 @@ def gen_zones(config):
     return zones
 
 
+def groupby_zones(capteurs, config):
+    zones = [[] for i in range(config["nb_zone"])]
+    for capteur in capteurs:
+        for zone in capteurs[capteur][1]:
+            zones[zone].append(capteur)
+    return zones
+
+
 def groupby_capteur(zones, config):
     capteurs = dict()
     for i in range(config["nb_capteur"]):
-        capteurs[i] = [zones.index(zone) for zone in zones
-                       if zone.count(i)]
+        capteurs[i] = [j for j in range(0, len(zones))
+                       if zones[j].count(i)]
         capteurs[i] = [random.randint(1, 10), capteurs[i]]
     return capteurs
+
+
+def super_recursion_of_death(zones, capteurs):
+    importants_plots = [zone[0] for zone in zones if len(zone) == 1]
+    # Remove duplicates
+    importants_plots = list(set(importants_plots))
+    useless_plots = capteurs.copy()
+    for key in importants_plots:
+        useless_plots.pop(key, None)
 
 
 with open('config.json') as data_file:
@@ -36,4 +56,8 @@ pprint(config)
 zones = gen_zones(config)
 capteurs = groupby_capteur(zones, config)
 print("Capteurs : " + str(capteurs))
+print("Zones : " + str(groupby_zones(capteurs, config)))
 print("Zones : " + str(zones))
+
+print("==================")
+super_recursion_of_death(zones, capteurs)
